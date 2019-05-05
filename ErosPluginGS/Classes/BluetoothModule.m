b6fae903a55a8534b929e02246320335920c7f32
晓峰 <32335@163.com>
2019-05-05 15:50:45 +0800

    重写打印参数

--- ErosPluginGS/Classes/BluetoothModule.m
+++ ErosPluginGS/Classes/BluetoothModule.m
@@ -35,8 +35,8 @@ WX_PlUGIN_EXPORT_MODULE(BluetoothModule, BluetoothModule)
 @implementation BluetoothModule
 
 // 将方法暴露出去
-WX_EXPORT_METHOD(@selector(isSupport:))
-WX_EXPORT_METHOD(@selector(isEnabled:))
+WX_EXPORT_METHOD_SYNC(@selector(isSupport))
+WX_EXPORT_METHOD_SYNC(@selector(isEnabled))
 WX_EXPORT_METHOD(@selector(searchDevices:endCallBack:))
 WX_EXPORT_METHOD(@selector(queryTsc:))
 WX_EXPORT_METHOD(@selector(disconnectPrinter))
@@ -69,17 +69,17 @@ WX_EXPORT_METHOD(@selector(printLabel:callback:))
 /**
  * 是否支持蓝牙设备
  */
-- (void)isSupport:(WXModuleCallback)callback {
+- (BOOL)isSupport {
     [self.bluetoothManager.delegate centralManagerDidUpdateState:self.bluetoothManager];
-    callback(_support);
+    return _support;
 }
 
 /**
  * 蓝牙是否启用
  */
-- (void)isEnabled:(WXModuleCallback)callback {
+- (BOOL)isEnabled {
     [self.bluetoothManager.delegate centralManagerDidUpdateState:self.bluetoothManager];
-    callback(_enable);
+    return _enable;
 }
 
 /**
@@ -120,7 +120,9 @@ WX_EXPORT_METHOD(@selector(printLabel:callback:))
     };
     [Manager write:data];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        callback(dataStr);
+        if([@"99" isEqualToString:dataStr]) {
+            callback(dataStr);
+        }
     });
 
 }
@@ -252,176 +254,152 @@ WX_EXPORT_METHOD(@selector(printLabel:callback:))
 //    callback(@"true");
 }
 
-/**
- {
- "width": 750,
- "height": 50,
- "gap": 2,
- "direction": 0,
- "density": 3,
- "mirror": 0,
- "speed": 1,
- "reference": [
- 0,
- 0
- ],
- "tear": 0,
- "sound": 0,
- "address": "DC:0D:30:04:33:69",
- "reverse": [{
- "x": 0,
- "y": 0,
- "width": 0,
- "height": 0
- }],
- "text": [{
- "text": "I am a testing txt",
- "x": 20,
- "y": 10,
- "fonttype": "TSS24.BF2",
- "rotation": 0,
- "xscal": 1,
- "yscal": 1
- },
- {
- "text": "你在说什么呢?",
- "x": 20,
- "y": 50,
- "fonttype": "TSS24.BF2",
- "rotation": 0,
- "xscal": 1,
- "yscal": 1,
- "bold": true
- }
- ],
- "qrcode": [{
- "x": 20,
- "y": 100,
- "level": "L",
- "width": 3,
- "rotation": 0,
- "code": "show me the money"
- }],
- "barcode": [{
- "x": 20,
- "y": 300,
- "type": "128",
- "height": 40,
- "readabel": 1,
- "rotation": 0,
- "code": "1234567890"
- }],
- "image": [{
- "x": 300,
- "y": 10,
- "mode": 0,
- "width": 200,
- "image": "iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAA8FBMVEUAAABCQkJDQ0NFRUU/Pz9BQUFAQEBERERDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0MAAAA0ZZMIAAAATnRSTlMAAAAAAAAAABWFz8JdBQFHt9OYIxSi/PBsBFHjvCSk/vJt5b7mo26h75ziIZkD1csRXvpziwvx+QadveRSSA3XF6r31DMPOSLWzMTZFgd4wftfAAAAAWJLR0QAiAUdSAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAaBJREFUSMe11dlSwjAUgOE2WmUTQRBtBQVBREREQEVUFkHcz/s/jklbQ7YOhwtz2fzftJ1OTi0rWDaJxRPJ1A6xxEXSu5nsXo7Ylrpskt8vABwcuqIgG94RABRLmtgk+eMTugXliiAI8U7ZRaiqwvnrJUH7WnBRFfR5zsKeinoohN4XRHyeZc8F2RJ6SSh9KJReeCpH7QOh9st76L3/5lrPRf5c6wEaF039IlQvmYgXAL1aVxQk8D20YxQk1wDXHQpuGui+22Pv4FbK2L5/639Rt44TYY8WvEcKoUcJqUcIpV8ptN4Xd5H9vd5TMXiIBMOOoXe8x0igzJKgf6pB9JJmCaIXJkPYb6/oFYHoJYHqxXllo/qlcDxcz8VzE9lTkWInLoPuAZIjCrJrgPGEgtYaYDqgIFc07LwMTbNkNmfvQEpVbafbfzXMkvbCn622Lth50adP2BuEf740MVvwP4oi+LyShNArQphXgpB69v/jQppXXCi9IJR5FQqt50KbV74w9Ey8td4/etq8Sn1+TeeGngn3u5PW7myPJj/G/v/WL4DMswebZ4AxAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE1LTA2LTI1VDA4OjQ0OjQ2KzA4OjAww1b9dwAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxNS0wNi0yNVQwODo0NDo0NiswODowMLILRcsAAAAASUVORK5CYII="
- }]
- }
- */
--(NSData *)tscCommand:(NSDictionary *)info{
+-(float)findSpeedValue:(NSInteger) sp{
+    // SPEED1DIV5(1.5F), SPEED2(2.0F), SPEED3(3.0F), SPEED4(4.0F);
+    switch (sp) {
+            case 2:
+            return 2.0f;
+            break;
+            case 3:
+            return 3.0f;
+            break;
+            case 4:
+            return 4.0f;
+            break;
+            case 1:
+        default:
+            return 1.5f;
+            break;
+    }
+}
+
+-(NSData *)tscCommand:(NSDictionary *)options{
+    NSInteger width = [[options valueForKey:@"width"] integerValue];
+    NSInteger height = [[options valueForKey:@"height"] integerValue];
+    NSInteger gap = [[options valueForKey:@"gap"] integerValue];
+    NSInteger home = [[options valueForKey:@"home"] integerValue];
+    NSString *tear = [options valueForKey:@"tear"];
+    if(!tear || ![@"ON" isEqualToString:tear]) tear = @"OFF";
+    NSArray *texts = [options objectForKey:@"text"];
+    NSArray *qrCodes = [options objectForKey:@"qrcode"];
+    NSArray *barCodes = [options objectForKey:@"barcode"];
+    NSArray *images = [options objectForKey:@"image"];
+    NSArray *reverses = [options objectForKey:@"revers"];
+    NSInteger direction = [[options valueForKey:@"direction"] integerValue];
+    NSInteger density = [[options valueForKey:@"density"] integerValue];
+    NSArray* reference = [options objectForKey:@"reference"];
+    NSInteger sound = [[options valueForKey:@"sound"] integerValue];
+    NSInteger speed = [[options valueForKey:@"speed"] integerValue];
+
+    TscCommand *tsc = [[TscCommand alloc] init];
+    if(speed){
+        [tsc addSpeed:[self findSpeedValue:speed]];
+    }
+    if(density){
+        [tsc addDensity:density];
+    }
+    [tsc addSize:width :height];
+    [tsc addGapWithM:gap withN:0];
+    [tsc addDirection:direction];
+    if(reference && [reference count] ==2){
+        NSInteger x = [[reference objectAtIndex:0] integerValue];
+        NSInteger y = [[reference objectAtIndex:1] integerValue];
+        NSLog(@"refernce  %ld y:%ld ",x,y);
+        [tsc addReference:x :y];
+    }else{
+        [tsc addReference:0 :0];
+    }
+ 
+    [tsc addTear:tear];
+//    if(home && home == 1){
+//        [tsc addBackFeed:16];
+//        [tsc addHome];
+//    }
+    [tsc addCls];
     
-    TscCommand *command = [[TscCommand alloc]init];
-    [command addCls];
-    //设置标签尺寸的宽和高
-    if ([info objectForKey:@"width"] && [info objectForKey:@"height"]) {
-        [command addSize:[[info objectForKey:@"width"] intValue] :[[info objectForKey:@"height"] intValue]];
-    }
-    //设置标签间隙尺寸 单位mm
-    if ([info objectForKey:@"gap"]) {
-        [command addGapWithM:[[info objectForKey:@"gap"] intValue] withN:0];
-    }
-    //打印方向
-    if ([info objectForKey:@"direction"]) {
-        [command addDirection:[[info objectForKey:@"direction"] intValue]];
-    }
-    //打印浓度
-    if ([info objectForKey:@"density"]) {
-        [command addDensity:[[info objectForKey:@"density"] intValue]];
-    }
-    //mirror
-    //设置打印速度
-    if ([info objectForKey:@"speed"]) {
-        [command addSpeed:[[info objectForKey:@"speed"] intValue]];
-    }
-    //设置标签原点坐标
-    if ([info objectForKey:@"reference"]) {
-        NSArray *arr = [info objectForKey:@"reference"];
-        if ([arr count] == 0) {
-            [command addReference:[[arr objectAtIndex:0] intValue] :[[arr objectAtIndex:1] intValue]];
-        }else {
-            [command addReference:0 :0];
-        }
-    }
-    //设置打印机撕离模式
-    if ([info objectForKey:@"tear"]) {
-        if([[info objectForKey:@"tear"] intValue] == 0){
-            [command addTear:@"OFF"];
-        }else if([[info objectForKey:@"tear"] intValue] == 1) {
-            [command addTear:@"ON"];
-        }else {
-            [command addTear:@"OFF"];
-        }
-    }
-    //设置蜂鸣器
-    if ([info objectForKey:@"sound"]) {
-        [command addSound:[[info objectForKey:@"sound"] intValue] :0];
-    }
-    //address
-    //将指定的区域反向打印（黑色变成白色，白色变成黑色）
-    if ([info objectForKey:@"reverse"]) {
-        NSDictionary *dic =[[info objectForKey:@"reverse"] objectAtIndex:0];
-        [command addReverse:[[dic objectForKey:@"x"] intValue] :[[dic objectForKey:@"y"] intValue] :[[dic objectForKey:@"width"] intValue] :[[dic objectForKey:@"height"] intValue]];
-    }else {
-        [command addReverse:0 :0 :0 :0];
-    }
-    //在标签上绘制文字
-    if ([info objectForKey:@"text"]) {
-        NSArray *arr =[info objectForKey:@"text"];
-        for (int i = 0; i < [arr count]; i ++) {
-            NSDictionary *dic = [arr objectAtIndex:i];
-            [command addTextwithX:[[dic objectForKey:@"x"] intValue] withY:[[dic objectForKey:@"y"] intValue] withFont:[dic objectForKey:@"fonttype"] withRotation:[[dic objectForKey:@"rotation"] intValue] withXscal:[[dic objectForKey:@"xscal"] intValue] withYscal:[[dic objectForKey:@"yscal"] intValue] withText:[dic objectForKey:@"text"]];
-        }
+    //Add Texts
+    for(int i=0; texts && i<[texts count];i++){
+        NSDictionary * text = [texts objectAtIndex:i];
+        NSString *t = [text valueForKey:@"text"];
+        NSInteger x = [[text valueForKey:@"x"] integerValue];
+        NSInteger y = [[text valueForKey:@"y"] integerValue];
+        NSString *fontType = [text valueForKey:@"fonttype"];
+        NSInteger rotation = [[text valueForKey:@"rotation"] integerValue];
+        NSInteger xscal = [[text valueForKey:@"xscal"] integerValue];
+        NSInteger yscal = [[text valueForKey:@"yscal"] integerValue];
+        Boolean bold = [[text valueForKey:@"bold"] boolValue];
         
-    }
-    //在标签上绘制QRCode二维码
-    if ([info objectForKey:@"qrcode"]) {
-        NSArray *arr =[info objectForKey:@"qrcode"];
-        for (int i = 0; i < [arr count]; i ++) {
-            NSDictionary *dic = [arr objectAtIndex:i];
-            [command addQRCode:[[dic objectForKey:@"x"] intValue]  :[[dic objectForKey:@"y"] intValue] :[dic objectForKey:@"level"] :[[dic objectForKey:@"width"] intValue] :@"A" :[[dic objectForKey:@"rotation"] intValue] :[dic objectForKey:@"code"]];
+        [tsc addTextwithX:x withY:y withFont:fontType withRotation:rotation withXscal:xscal withYscal:yscal withText:t];
+        if(bold){
+            [tsc addTextwithX:x+1 withY:y withFont:fontType withRotation:rotation withXscal:xscal withYscal:yscal withText:t];
+            [tsc addTextwithX:x withY:y+1 withFont:fontType withRotation:rotation withXscal:xscal withYscal:yscal withText:t];
         }
     }
-    //在标签上绘制一维条码
-    if ([info objectForKey:@"barcode"]) {
-        NSArray *arr =[info objectForKey:@"barcode"];
-        for (int i = 0; i < [arr count]; i ++) {
-            NSDictionary *dic = [arr objectAtIndex:i];
-            [command add1DBarcode:[[dic objectForKey:@"x"] intValue]  :[[dic objectForKey:@"y"] intValue] :[NSString stringWithFormat:@"CODE%@", [dic objectForKey:@"type"]] :[[dic objectForKey:@"height"] intValue] :[[dic objectForKey:@"readabel"] intValue] :[[dic objectForKey:@"rotation"] intValue] :2 :2 :[dic objectForKey:@"code"]];
-        }
+    
+    //images
+    for (int i = 0; images && i < [images count]; i++) {
+        NSDictionary *img = [images objectAtIndex:i];
+        NSInteger x = [[img valueForKey:@"x"] integerValue];
+        NSInteger y = [[img valueForKey:@"y"] integerValue];
+        NSInteger imgWidth = [[img valueForKey:@"width"] integerValue];
+        NSInteger mode = [[img valueForKey:@"mode"] integerValue];
+        NSString *image  = [img valueForKey:@"image"];
+        NSData *imageData = [[NSData alloc] initWithBase64EncodedString:image options:0];
+        UIImage *uiImage = [[UIImage alloc] initWithData:imageData];
+        
+        [tsc addBitmapwithX:x withY:y withMode:mode withWidth:imgWidth withImage:uiImage];
     }
-    //图片
-    if ([info objectForKey:@"image"]) {
-        NSArray *arr =[info objectForKey:@"image"];
-        for (int i = 0; i < [arr count]; i ++) {
-            NSDictionary *dic = [arr objectAtIndex:i];
-//            UIImage *image = [self decodeBase64ToImage:[dic objectForKey:@"image"]];
-            NSString *strImg = [dic objectForKey:@"image"];
-            NSData *imageData = [NSData dataFromBase64String:strImg];
-            UIImage *image = [[UIImage alloc] initWithData:imageData];
-//            UIImage *image = [UIImage imageNamed:@"gprinter.png"];
-            [command addBitmapwithX:[[dic objectForKey:@"x"] intValue]  withY:[[dic objectForKey:@"y"] intValue]  withMode:[[dic objectForKey:@"mode"] intValue]  withWidth:[[dic objectForKey:@"width"] intValue] withImage:image];
-        }
+    
+    //QRCode
+    for (int i = 0; qrCodes && i < [qrCodes count]; i++) {
+        NSDictionary *qr = [qrCodes objectAtIndex:i];
+        NSInteger x = [[qr valueForKey:@"x"] integerValue];
+        NSInteger y = [[qr valueForKey:@"y"] integerValue];
+        NSInteger qrWidth = [[qr valueForKey:@"width"] integerValue];
+        NSString *level = [qr valueForKey:@"level"];
+        if(!level)level = @"M";
+        NSInteger rotation = [[qr valueForKey:@"rotation"] integerValue];
+        NSString *code = [qr valueForKey:@"code"];
+
+        [tsc addQRCode:x :y :level :qrWidth :@"A" :rotation :code];
     }
-    if ([info objectForKey:@"count"]) {
-        [command addPrint:[[info objectForKey:@"count"] intValue] :1];
+    
+    //BarCode
+    for (int i = 0; barCodes && i < [barCodes count]; i++) {
+        NSDictionary *bar = [barCodes objectAtIndex:i];
+        NSInteger x = [[bar valueForKey:@"x"] integerValue];
+        NSInteger y = [[bar valueForKey:@"y"] integerValue];
+        NSInteger barWide =[[bar valueForKey:@"wide"] integerValue];
+        if(!barWide) barWide = 2;
+        NSInteger barHeight = [[bar valueForKey:@"height"] integerValue];
+        NSInteger narrow = [[bar valueForKey:@"narrow"] integerValue];
+        if(!narrow) narrow = 2;
+        NSInteger rotation = [[bar valueForKey:@"rotation"] integerValue];
+        NSString *code = [bar valueForKey:@"code"];
+        NSString *type = [bar valueForKey:@"type"];
+        NSInteger readable = [[bar valueForKey:@"readable"] integerValue];
+        
+        [tsc add1DBarcode:x :y :type :barHeight :readable :rotation :narrow :barWide :code];
+    }
+    for(int i=0; reverses&& i < [reverses count]; i++){
+        NSDictionary *area = [reverses objectAtIndex:i];
+        NSInteger ax = [[area valueForKey:@"x"] integerValue];
+        NSInteger ay = [[area valueForKey:@"y"] integerValue];
+        NSInteger aWidth = [[area valueForKey:@"width"] integerValue];
+        NSInteger aHeight = [[area valueForKey:@"height"] integerValue];
+
+        [tsc addReverse:ax :ay :aWidth :aHeight];
+    }
+    
+    if ([options objectForKey:@"count"]) {
+        [tsc addPrint:[[options objectForKey:@"count"] intValue] :1];
     }else {
-        [command addPrint:1 :1];
+        [tsc addPrint:1 :1];
     }
-    
-    return [command getCommand];
-    
+    if (sound) {
+        [tsc addSound:2 :100];
+    }
+    return [tsc getCommand];
 }
 
 
